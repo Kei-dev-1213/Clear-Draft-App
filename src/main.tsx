90b5fe1338916af849fa0bf5b89f992047f5f1a8
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-20 21:21:27 +0900

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { theme } from "./Theme.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./theme.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
