f7a228b6ddd42b9ea67b433ca67d59a92cb5a4d6
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-30 20:34:46 +0900

    work 8/30

---
+++ src/page/Settings/Settings.test.tsx
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { waitFor } from "@testing-library/react";
+
+/*
+ * 設定画面のテスト
+ */
+describe("サンプルテスト", () => {
+  test("[正常系]サンプルテスト", async () => {
+    // 実行
+    // render(<App />);
+
+    // 検証
+    waitFor(() => {});
+  });
+});
