9fd4d24712fe58bfde89cc8a0f0b8daa0303a479
Kei-dev-1213 <zz2003live@gmail.com>
2024-09-01 20:47:34 +0900

    work 9/1

--- src/page/Settings/Settings.test.tsx
+++ src/page/Settings/Settings.test.tsx
@@ -1,7 +1,21 @@
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { Settings } from "./Settings";
+import userEvent from "@testing-library/user-event";
+import { LoadingProvider } from "../../context/LoadingProvider";
+import { DB } from "../../supabase";
 
+// モック化
+jest.mock("../../context/LoadingProvider", () => {
+  const originalModule = jest.requireActual("../../context/LoadingProvider");
+  return {
+    __esModule: true,
+    ...originalModule,
+    ParentContext: {
+      _currentValue: { value1: "mockValue1", value2: "mockValue2" },
+    },
+  };
+});
 jest.mock("../../supabase", () => ({
   DB: {
     registQiitaAPIKey: jest.fn(),
@@ -21,22 +35,24 @@ describe("初期表示テスト", () => {
   });
 });
 
-describe("初期表示テスト", () => {
-  test("[正常系]入力項目が空であること", async () => {
-    // 実行
-    render(<Settings />);
-    // 検証
-    const apiInput = screen.getByTestId("qiita-api-input");
-    expect(apiInput).toHaveValue("");
-  });
-});
-
 describe("登録テスト", () => {
   test("[正常系]登録処理が正常に動作すること", async () => {
     // 実行
-    render(<Settings />);
-    // 検証
+    render(<Settings />, { wrapper: LoadingProvider });
+
+    // 入力
+    const token = "test-api-key";
     const apiInput = screen.getByTestId("qiita-api-input");
+    await userEvent.type(apiInput, token);
+    // 登録
+    const registButton = screen.getByTestId("regist-button");
+    fireEvent.click(registButton);
+
+    // 検証
+    await waitFor(async () => {
+      expect(DB.registQiitaAPIKey).toHaveBeenCalledTimes(1);
+      expect(DB.registQiitaAPIKey).not.toHaveBeenCalledWith(token);
+    });
     expect(apiInput).toHaveValue("");
   });
 
